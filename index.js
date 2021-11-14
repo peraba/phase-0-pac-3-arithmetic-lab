2e78c82f5f2efde6073b484829c5a916638164f2
Pedro Ramos <medpsr9@gmail.com>
2021-11-14 17:07:54 -0600

    Completed assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+let number = 5;
+number += 3;
+number -= 2;
+number *= 10;
+number /= 5;
+
+parseInt("2", 10);
+makeInt(string)
+parseInt("2", 10);
+preserveDecimal(string)
+parseFloat("80.123999");
